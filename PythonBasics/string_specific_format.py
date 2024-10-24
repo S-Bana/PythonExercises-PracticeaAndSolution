1419c4a2886ec3ef5e282140edb9e4b30359ca64
Sajad <sajadbanazadeh@gmail.com>
2024-10-25 00:04:44 +0330

    code specific format string is complate

--- PythonBasics/string_specific_format.py
+++ PythonBasics/string_specific_format.py
@@ -10,3 +10,5 @@ Twinkle, twinkle, little star,
 Twinkle, twinkle, little star, 
 	How I wonder what you are
 '''
+
+print("Twinkle, twinkle, little star, \n\tHow I wonder what you are! \n\t\tUp above the world so high, \n\t\tLike a diamond in the sky. \nTwinkle, twinkle, little star, \n\tHow I wonder what you are!")
